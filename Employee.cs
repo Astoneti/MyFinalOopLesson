a23b4541858a3a0c6854f4de4832a82324d90fdd
a.pashkun@godeltech.com <a.pashkun@godeltech.com>
2021-01-26 19:43:50 +0300

    MY final OOP lesson:)

---
+++ Employee.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalOopLesson
+{
+    class Employee
+    {
+        private decimal amount;
+        private double rate;
+        private decimal experience;
+        private decimal hourlyPaymentCount;
+        private decimal sickLeaveCount;
+        
+        public string Title { get; set; } 
+        public string Position { get; set; }
+        public decimal Amount 
+        {
+            get { return amount; }
+            set 
+            {
+                if (value >= 0)
+                {
+                    amount = value;
+                }
+                else
+                if (value < 0) throw new Exception("Invalid error!, enter the correct value > 0");
+            } 
+        }
+        public double Rate
+        {
+            get { return rate; }
+            set
+            {
+                if (value >= 0)
+                {
+                    rate = value;
+                }
+                else
+                if (value < 0) throw new Exception("Invalid error!, enter the correct value > 0");
+            }
+        }
+        public decimal Experience
+        {
+            get { return experience; }
+            set
+            {
+                if (value >= 0)
+                {
+                    experience = value;
+                }
+                else
+                if (value < 0) throw new Exception("Invalid error!, enter the correct value > 0");
+            }
+        }
+        public decimal HourlyPaymentCount
+        {
+            get { return hourlyPaymentCount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    hourlyPaymentCount = value;
+                }
+                else
+                if (value < 0) throw new Exception("Invalid error!, enter the correct value > 0");
+            }
+        }
+        public decimal SickLeaveCount
+        {
+            get { return sickLeaveCount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    sickLeaveCount = value;
+                }
+                else
+                if (value < 0) throw new Exception("Invalid error!, enter the correct value > 0");
+            }
+        }
+
+        public Employee() { }
+        public Employee(string title, string position) : this() 
+        {
+            Title = title;
+            Position = position;
+        }
+
+        public Employee(string title, string position, decimal amount, double rate, decimal experience, decimal hourlyPaymentCount, decimal sickLeaveCount) : this( title, position)
+        {
+            Amount = amount;
+            Rate = rate;
+            Experience = experience;
+            HourlyPaymentCount = hourlyPaymentCount;
+            SickLeaveCount = sickLeaveCount;
+        }
+
+        public static Employee GetMostPaidEmployee(Employee salary1, Employee salary2, Employee salary3)    
+        {
+            decimal sum1 = salary1.GetSalaryCalculation();  
+            decimal sum2 = salary2.GetSalaryCalculation();    
+            decimal sum3 = salary3.GetSalaryCalculation(); 
+            return (sum1 > sum2) ? ((sum1 > sum3) ? salary1 : salary3) : ((sum2 > sum3) ? salary2 : salary3);
+        }
+
+        public bool ShowIsSalaryBigger(Employee salary) 
+        {
+            return amount > salary.amount;
+        }
+
+        public decimal GetSalaryCalculation()
+        {
+            decimal salary = (decimal) rate * amount;
+            decimal hourAmount = GetSalaryBasedOnHourlyPaid();
+            decimal experienceAmount = GetSalaryBasedOnExperience(salary);
+            return salary + experienceAmount + hourAmount;
+        }
+        
+        public override string ToString()
+        {
+            return string.Format("Title: {0}, Position: {1}, Amount: {2}, Rate: {3}, Experience: {4}, HourlyPaymentCount: {5}, SickLeaveCount: {6}", Title, Position, Amount, Rate, Experience, HourlyPaymentCount, SickLeaveCount);
+        }
+
+        private decimal GetSalaryBasedOnHourlyPaid()     
+        {
+            decimal hourSalary = hourlyPaymentCount * 10;
+            decimal totalSum = amount + hourlyPaymentCount;
+            return totalSum;
+        }
+
+        private decimal GetSalaryBasedOnExperience (decimal salary)      
+        {
+            decimal result = 0;
+            if (experience > 10)
+            {
+                result = (salary / 100) * 10;
+            }
+            if (experience > 20)
+            {
+                result = (salary / 100) * 20;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FinalOopLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee developer = new Employee {Title = "developer", Position = ".Net Devision", Amount = 3500, Rate = 1.75, Experience = 10, HourlyPaymentCount = 240, SickLeaveCount = 23 };
+            Employee hR = new Employee{ Title = "HR manager", Position = "HR Devision", Amount = 1350, Rate =  1.65, Experience = 7, HourlyPaymentCount = 230, SickLeaveCount = 19 };
+            Employee driver = new Employee { Title = "corparate driver", Position = "Administration", Amount = 800, Rate = 1.66, Experience = 15, HourlyPaymentCount = 220, SickLeaveCount = 11 };
+
+            Employee employeeInfo1 = new Employee();
+            Employee employeeInfo2 = new Employee("developer", ".Net Devision");
+            Employee employeeInfo3 = new Employee("developer", ".Net Devision", 3500, 1.75, 5, 240, 23);
+           
+            bool isFirstSalarybiggerThanSecond = developer.ShowIsSalaryBigger(hR);
+            developer.Amount = 3500;
+            hR.Amount = 1350;
+            driver.Amount = 800;
+
+            var mostPaidEmployee = Employee.GetMostPaidEmployee(developer,hR,driver);
+
+            decimal salarySum1 = developer.GetSalaryCalculation();
+            decimal salarySum2 = hR.GetSalaryCalculation();
+            decimal salarySum3 = driver.GetSalaryCalculation();
+        }
+    }
+}
